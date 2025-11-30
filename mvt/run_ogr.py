63e70294f7885cd22f9c711c52e9c2b25e656aba
jole84 <johanlennartsson84@gmail.com>
2025-11-30 11:04:04 +0100

    Update run_ogr.py

--- mvt/run_ogr.py
+++ mvt/run_ogr.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-import os, shutil
+from os import system, chdir
+from shutil import rmtree
 
-os.chdir('/home/johan/git/jole84-maptile/ol-webmap')
-# os.chdir('/home/johan/Karta')
+# minzoom 12 syns på >13 (+1)
+# maxzoom 8 släcks på >10 (+2)
 
-try:
-    shutil.rmtree('tiles')
-except OSError as e:
-    print(e.strerror + ":", e.filename)
+chdir('/home/johan/git/jole84-maptile/ol-webmap')
+# chdir('/home/johan/Karta')
 
-clipsrc = ""
-clipsrc = "-clipsrc 1550000 7900000 1650000 8000000" # jönköping      31s 8,4mb
-# clipsrc = "-clipsrc 1450000 7800000 1700000 8100000" # jönköping++    150s 180mb      166s 155mb
+# clipsrc = ""
+clipsrc = "-clipsrc 1550000 7900000 1650000 8000000" # jönköping  31s 8,4mb     1m40s
+# clipsrc = "-clipsrc 1450000 7800000 1700000 8100000" # jönköping++    150s 180mb   
 # clipsrc = "-clipsrc 1200000 7400000 2200000 8350000" # götaland      
 # sverige   48min   2,7gb   43min 2,3gb
 
+try:
+    rmtree('tiles')
+except OSError as e:
+    print(e.strerror + ":", e.filename)
+
 cmd = " ".join([
     'time',
-    'echo',
     'ogr2ogr tiles/ -f MVT',
     clipsrc,
     '-t_srs "EPSG:3857"',
@@ -27,18 +30,13 @@ cmd = " ".join([
     '-dsco SIMPLIFICATION=5',
     '-dsco SIMPLIFICATION_MAX_ZOOM=2',
     '-dsco COMPRESS=NO',
-    '-dsco conf=layerconf.json',
-    '$HOME/git/jole84-maptile/mvt/layers.vrt',
+    '-dsco conf=/home/johan/git/jole84-maptile/mvt/layerconf.json',
+    '/home/johan/git/jole84-maptile/mvt/layers.vrt',
     ])
 
-os.system(cmd)
+system(cmd)
 
-# minzoom 12 syns på >13 (+1)
-# maxzoom 8 släcks på >10 (+2)
-
-# rsync -aPhz --info=progress2 --progress tiles root@192.46.232.175:/var/www/html/ --delete
-
-runRsync = input("kör rsync? [y] ").lower()
-
-if (runRsync == "y"):
-    os.system("rsync -aPhz --info=progress2 --progress tiles root@192.46.232.175:/var/www/html/ --delete")
+if clipsrc == "":
+    runRsync = input("kör rsync? [y] ").lower()
+    if (runRsync == "y"):
+        system("rsync -aPhz --info=progress2 --progress tiles root@192.46.232.175:/var/www/html/ --delete")
