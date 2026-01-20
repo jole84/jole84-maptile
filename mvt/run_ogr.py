0a0822ed1dafcf67bf9cab73521a6b415baa9791
jole84 <johanlennartsson84@gmail.com>
2026-01-20 18:42:10 +0100

    Update run_ogr.py

--- mvt/run_ogr.py
+++ mvt/run_ogr.py
@@ -9,7 +9,7 @@ chdir('/home/johan/git/jole84-maptile/ol-webmap')
 # chdir('/home/johan/Karta')
 
 # clipsrc = ""
-clipsrc = "-clipsrc 1550000 7900000 1650000 8000000" # jönköping  31s 8,4mb     1m40s
+clipsrc = "-clipsrc 1550000 7900000 1650000 8000000" # jönköping  128s 19,9mb (simp0 125s 22,3mb)
 # clipsrc = "-clipsrc 1450000 7800000 1700000 8100000" # jönköping++    150s 180mb   
 # clipsrc = "-clipsrc 1200000 7400000 2200000 8350000" # götaland      
 # sverige   48min   2,7gb   43min 2,3gb
@@ -28,7 +28,7 @@ cmd = " ".join([
     '-dsco MINZOOM=5',
     '-dsco MAXZOOM=14',
     '-dsco SIMPLIFICATION=5',
-    '-dsco SIMPLIFICATION_MAX_ZOOM=2',
+    # '-dsco SIMPLIFICATION_MAX_ZOOM=2',
     '-dsco COMPRESS=NO',
     '-dsco conf=/home/johan/git/jole84-maptile/mvt/layerconf.json',
     '/home/johan/git/jole84-maptile/mvt/layers.vrt',
@@ -40,3 +40,10 @@ if clipsrc == "":
     runRsync = input("kör rsync? [y] ").lower()
     if (runRsync == "y"):
         system("rsync -aPhz --info=progress2 --progress tiles root@192.46.232.175:/var/www/html/ --delete")
+
+
+
+# minzoom 12 syns på >13 (+1)
+# maxzoom 8 släcks på >10 (+2)
+
+# rsync -aPhz --info=progress2 --progress tiles root@192.46.232.175:/var/www/html/ --delete
