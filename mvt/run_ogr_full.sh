b0fb36025ded5f4d51b4d50446ccc71e10b79771
jole84 <johanlennartsson84@gmail.com>
2026-06-02 19:54:10 +0200

    run_ogr name change

--- mvt/run_ogr_full.sh
+++ mvt/run_ogr_full.sh
@@ -2,14 +2,13 @@
 
 rm tiles/ -r
 
-# clipsrc = "-clipsrc 1550000 7900000 1650000 8000000" # jönköping      31s 8,4mb
-# clipsrc = "-clipsrc 1450000 7800000 1700000 8100000" # jönköping++    150s 180mb      166s 155mb
-# clipsrc = "-clipsrc 1200000 7400000 2200000 8350000" # götaland      
+# -spat 1550000 7900000 1650000 8000000 # jönköping      31s 8,4mb
+# -spat 1450000 7800000 1700000 8100000 # jönköping++    150s 180mb      166s 155mb
+# -spat 1200000 7400000 2200000 8350000 # götaland      
 # sverige   48min   2,7gb   43min 2,3gb
 # -dsco SIMPLIFICATION=50 \
 
 ogr2ogr tiles/ -f MVT \
-    -spat 1550000 7900000 1650000 8000000 \
     -t_srs "EPSG:3857" \
     -skipfailures \
     -dsco MINZOOM=5 \
@@ -20,8 +19,6 @@ ogr2ogr tiles/ -f MVT \
     -dsco conf=$HOME/git/jole84-maptile/mvt/layerconf.json \
     $HOME/git/jole84-maptile/mvt/layers.vrt
 
-
-# TNE_FT_VAGDATA_SIMPLIFIED
 
 # minzoom 12 syns på >13 (+1)
 # maxzoom 8 släcks på >10 (+2)
